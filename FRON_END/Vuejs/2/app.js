5cbaf062aca1c395b019a9ae3b7d1895d52a4ad4
50027 <50027@hfz.com>
2022-07-17 10:11:19 +0430

    modified format code by prettier

--- FRON_END/Vuejs/2/app.js
+++ FRON_END/Vuejs/2/app.js
@@ -1,19 +1,17 @@
 Vue.createApp({
-    data() {
-        return{
-            goals: [],
-            enteredValue: ''
-        };
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
     },
-    methods:{
-        addGoal(){
-            this.goals.push(this.enteredValue);
-            this.enteredValue = '';
-        }
-    }
-}).mount('#app');
-
-
+  },
+}).mount("#app");
 
 // const buttonEl = document.querySelector('button');
 // const inputEl = document.querySelector('input');
