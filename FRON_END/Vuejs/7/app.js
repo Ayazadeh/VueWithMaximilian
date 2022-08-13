7a15dfa33450399fb36b9d8b3e7c4fee70cbd5a3
Ayazadeh <m.ayazadeh@gmail.com>
2022-08-13 08:10:10 +0430

    Vue2: Disable Re-Rendering with v-once

--- FRON_END/Vuejs/7/app.js
+++ FRON_END/Vuejs/7/app.js
@@ -3,12 +3,16 @@ new Vue({
   el: "#app",
   data: {
     title: "Hello World!",
-    link: 'http://google.com'
+    link: "http://google.com",
   },
   methods: {
     changeTitle(event) {
       // event: default parameter for func in vanilla js
       this.title = event.target.value;
+    },
+    sayHello() {
+      this.title = "Hello!";
+      return this.title;
     },
   },
 });
