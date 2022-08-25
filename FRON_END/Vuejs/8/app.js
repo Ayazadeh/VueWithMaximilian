c430535baf3548eeebebe622fa99def6e2f9d784
Ayazadeh <m.ayazadeh@gmail.com>
2022-08-25 14:43:54 +0430

    Vue2: add vue instance and some data & method to Monster Slayer project

--- FRON_END/Vuejs/8/app.js
+++ FRON_END/Vuejs/8/app.js
@@ -2,7 +2,19 @@
 new Vue({
   el: "#app",
   data: {
-   
+    playerHealth: 100,
+    monsterHealth: 100,
+    gameIsRunning: false,
   },
-  
+  methods: {
+    startGame() {
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    },
+    attack() {},
+    specialAttack() {},
+    heal() {},
+    giveUp() {},
+  },
 });
