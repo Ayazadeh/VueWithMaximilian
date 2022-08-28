8aefe8fe279ca00a87d2e9bbb55f9523a3d90448
Ayazadeh <m.ayazadeh@gmail.com>
2022-08-28 10:51:10 +0430

    Vue2: Monster Slayer (write attack function)

--- FRON_END/Vuejs/8/app.js
+++ FRON_END/Vuejs/8/app.js
@@ -12,9 +12,40 @@ new Vue({
       this.playerHealth = 100;
       this.monsterHealth = 100;
     },
-    attack() {},
+    attack() {
+      this.monsterHealth -= this.calculateDamage(3, 10);
+
+      if (this.checkWin()) {
+        return;
+      }
+
+      this.playerHealth -= this.calculateDamage(5, 12);
+
+      this.checkWin();
+    },
     specialAttack() {},
     heal() {},
     giveUp() {},
+    calculateDamage(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin() {
+      if (this.monsterHealth <= 0) {
+        if (confirm("You won! New Game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm("You lost! New Game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    },
   },
 });
