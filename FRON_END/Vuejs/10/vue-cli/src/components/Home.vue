db92efc8ce55033f53d040dbe7256d549cea277d
Ayazadeh <m.ayazadeh@gmail.com>
2022-09-20 10:38:28 +0430

    Vue2: Scoping Component Styles

--- FRON_END/Vuejs/10/vue-cli/src/components/Home.vue
+++ FRON_END/Vuejs/10/vue-cli/src/components/Home.vue
@@ -12,3 +12,8 @@ export default {
   }
 };
 </script>
+<style scoped>
+div {
+  border: 1px solid blue; 
+}
+</style>
--- FRON_END/Vuejs/10/vue-cli/src/components/ServerStatus.vue
+++ FRON_END/Vuejs/10/vue-cli/src/components/ServerStatus.vue
@@ -19,3 +19,8 @@ export default {
   }
 };
 </script>
+<style scoped>
+div {
+  border: 2px solid red;
+}
+</style>
