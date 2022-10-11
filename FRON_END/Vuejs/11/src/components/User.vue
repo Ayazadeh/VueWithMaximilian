4931d23a83f3356783b532cb54312e81eb4d2c61
Ayazadeh <m.ayazadeh@gmail.com>
2022-10-11 19:37:02 +0330

    Vue2: make a new project

---
+++ FRON_END/Vuejs/11/src/components/User.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <button @click="changeName">Change my Name</button>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <app-user-detail></app-user-detail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit></app-user-edit>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import UserDetail from "./UserDetail.vue";
+import UserEdit from "./UserEdit.vue";
+
+export default {
+  component: {
+    appUserDetail: UserDetail,
+    appUserEdit: UserEdit
+  },
+  data() {
+    return {
+        name: 'Mohammad'
+    }
+  },
+  methods: {
+    changeName(){
+        this.name = 'Ayazadeh'
+    }
+  },
+};
+</script>
---
+++ FRON_END/Vuejs/11/src/components/UserDetail.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ name }}</p>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+div{
+    background-color: lightcoral;
+}
+</style>
