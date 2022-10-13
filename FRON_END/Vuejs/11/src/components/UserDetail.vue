8916bbea3e348b467e6c2ec2e81605e51b8c4fa2
Ayazadeh <m.ayazadeh@gmail.com>
2022-10-13 09:02:00 +0330

    Vue2: how to validate the props

--- FRON_END/Vuejs/11/src/components/UserDetail.vue
+++ FRON_END/Vuejs/11/src/components/UserDetail.vue
@@ -1,19 +1,61 @@
 <template>
-    <div class="component">
-        <h3>You may view the User Details here</h3>
-        <p>Many Details</p>
-        <p>User Name: {{ name }}</p>
-    </div>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{ myName }}</p>
+    <p>User Name Reverse: {{ switchName() }}</p>
+  </div>
 </template>
 <script>
 export default {
-    name: "UserDetail",
-    props: ['name']
-}
+  name: "UserDetail",
+
+  //   props: ["myName"],
+
+  //   props can have multiple types
+  //   props: {
+  //     myName: [String, Array]
+  //   },
+
+  // props can be an object like myName
+  // props: {
+  //   myName:{
+  //     type: String,
+  //     required: true
+  //   }
+  // },
+
+  // if we use Object type or Array type, default value will be like this
+  // props: {
+  //   myName: {
+  //     type: Object,
+  //     default: () => {
+  //       return {
+  //         name: "Max"
+  //       };
+  //     }
+  //   }
+  // },
+
+  props: {
+    myName: {
+      type: String,
+      default: "Max"
+    }
+  },
+  methods: {
+    switchName() {
+      return this.myName
+        .split("")
+        .reverse()
+        .join("");
+    }
+  }
+};
 </script>
 
 <style scoped>
-div{
-    background-color: lightcoral;
+div {
+  background-color: lightcoral;
 }
 </style>
