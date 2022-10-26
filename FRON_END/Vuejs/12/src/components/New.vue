3d6038b98dd3eec12c0f1856c32bb4042159849e
Ayazadeh <m.ayazadeh@gmail.com>
2022-10-26 08:24:44 +0330

    Vue2: Understanding Dynamic Component Behavior

--- FRON_END/Vuejs/12/src/components/New.vue
+++ FRON_END/Vuejs/12/src/components/New.vue
@@ -1,13 +1,25 @@
 <!-- parent component -->
 <template>
   <div>
-   <h3>The New</h3>
+    <h3>The New</h3>
+
+    <!-- Understanding Dynamic Component Behavior -->
+    <button @click="counter++">Increase!</button>
+    <p>{{ counter }}</p>
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  destroyed() {
+    // Understanding Dynamic Component Behavior
+    // when we change component, component will be destroyed & recreated
+    console.log("Destroyed!");
+  }
 };
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
