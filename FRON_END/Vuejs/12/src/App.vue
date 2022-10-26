72bbda57fd0f786453cade204ca940b04eedc50a
Ayazadeh <m.ayazadeh@gmail.com>
2022-10-26 08:53:22 +0330

    Vue2: End of Advanced Component Usage Part

--- FRON_END/Vuejs/12/src/App.vue
+++ FRON_END/Vuejs/12/src/App.vue
@@ -21,6 +21,30 @@
           <p slot="content">A wonderful Quote</p></p>
           <p slot="subtitle">subtitle from parent</p>
         </app-quote> -->
+
+        <br />
+        <button class="btn btn-primary" @click="selectedComponent2 = 'appBlue'">
+          Load Blue Template
+        </button>
+        <button class="btn btn-success" @click="selectedComponent2 = 'appGreen'">
+          Load Green Template
+        </button>
+        <button class="btn btn-danger" @click="selectedComponent2 = 'appRed'">
+          Load Red Template
+        </button>
+        <hr />
+        <component :is="selectedComponent2">
+          <p>This is the Content</p>
+        </component>
+        <!--<app-blue>-->
+        <!--<p>This is the Content</p>-->
+        <!--</app-blue>-->
+        <!--<app-green>-->
+        <!--<p>This is the Content</p>-->
+        <!--</app-green>-->
+        <!--<app-red>-->
+        <!--<p>This is the Content</p>-->
+        <!--</app-red>-->
       </div>
     </div>
   </div>
@@ -30,16 +54,24 @@
 import Quote from "@/components/Quote.vue";
 import Author from "@/components/Author.vue";
 import New from "@/components/New.vue";
+import Blue from "./components/Blue.vue";
+import Green from "./components/Green.vue";
+import Red from "./components/Red.vue";
+
 export default {
   components: {
     appQuote: Quote,
     appAuthor: Author,
-    appNew: New
+    appNew: New,
+    appBlue: Blue,
+    appGreen: Green,
+    appRed: Red
   },
   data() {
     return {
       quoteTitle: "The Quote",
-      selectedComponent: "appQuote"
+      selectedComponent: "appQuote",
+      selectedComponent2: "appBlue"
     };
   }
 };
