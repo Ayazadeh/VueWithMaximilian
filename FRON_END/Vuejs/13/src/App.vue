f81daa64e021a2dca40b23ff44f4db1ee98e15a3
Ayazadeh <m.ayazadeh@gmail.com>
2022-10-29 09:57:15 +0330

    Vue2: initializing the Application

--- FRON_END/Vuejs/13/src/App.vue
+++ FRON_END/Vuejs/13/src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
-
+    <h1>Mohammad Ayazadeh</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+  name: "App",
+  data() {
+    return {
+      quotes: ["Just a Quote to see something"],
+      maxQuotes: 10,
+    };
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arizonia;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
