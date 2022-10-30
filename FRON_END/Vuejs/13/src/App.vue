fec2a886a49635c200993364e0056245abe7e5fd
Ayazadeh <m.ayazadeh@gmail.com>
2022-10-30 09:54:49 +0330

    Vue2: Passing Data with Props and Slots

--- FRON_END/Vuejs/13/src/App.vue
+++ FRON_END/Vuejs/13/src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <!-- <div id="app"> -->
   <div class="container">
-    <h1>Mohammad Ayazadeh</h1>
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
   </div>
   <!-- </div> -->
 </template>
 
 <script>
+import QuoteGrid from "./components/QuoteGrid.vue";
 export default {
   name: "App",
+  components: {
+    appQuoteGrid: QuoteGrid,
+  },
   data() {
     return {
       quotes: ["Just a Quote to see something"],
--- FRON_END/Vuejs/13/src/components/QuoteGrid.vue
+++ FRON_END/Vuejs/13/src/components/QuoteGrid.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="row"></div>
+  <div class="row">
+    <app-quote v-for="quote in quotes">{{ quote }}</app-quote>
+  </div>
 </template>
 
 <script>
+import Quote from "./Quote.vue";
 export default {
+  components: {
+    appQuote: Quote,
+  },
   props: ["quotes"],
 };
 </script>
