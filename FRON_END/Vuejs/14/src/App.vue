2b048608fede5c468ca245bc32d927ada71fade0
Ayazadeh <m.ayazadeh@gmail.com>
2022-11-05 10:12:17 +0330

    Vue2: A Basic _input_ Form Binding

--- FRON_END/Vuejs/14/src/App.vue
+++ FRON_END/Vuejs/14/src/App.vue
@@ -1,28 +1,126 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div class="container">
+    <form action="">
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
+        >
+          <h1>File a Complaint</h1>
+          <hr />
+          <div class="form-group">
+            <label for="email">Mail</label>
+            <input
+              type="text"
+              id="email"
+              class="form-control"
+              v-model="email"
+            />
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" id="passsword" class="form-control" />
+          </div>
+          <div class="form-group">
+            <label for="age">Age</label>
+            <input type="number" id="age" class="form-control" />
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-affset-3 form-group"
+        >
+          <label for="message">Message</label>
+          <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work! -->
+          <textarea id="message" rows="5" class="form-control"></textarea>
+        </div>
+      </div>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col md-6 col-md-affset-3"
+        >
+          <div class="form-group">
+            <label for="sendmail">
+              <input type="checkbox" id="sendmail" value="SendMail" />
+              Send Mail
+            </label>
+            <label for="sendInfomail">
+              <input type="checkbox" id="sendInfomail" value="SendInfoMail" />
+              Send Infomail
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-affset-3 form-group"
+        >
+          <label for="male">
+            <input type="radio" id="male" value="Male" /> Male
+          </label>
+          <label for="female">
+            <input type="radio" id="femail" value="Female" /> Female
+          </label>
+        </div>
+      </div>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-affset-3 form-group"
+        >
+          <label for="priority">Priority</label>
+          <select id="priority" class="form-control">
+            <option></option>
+          </select>
+        </div>
+      </div>
+      <hr />
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-affset-3 form-group"
+        >
+          <button class="btn btn-primary">Submit!</button>
+        </div>
+      </div>
+    </form>
+    <hr />
+    <div class="row">
+      <div
+        class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-affset-3 form-group"
+      >
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4>Your Data</h4>
+          </div>
+          <div class="panel-body">
+            <p>Mail:{{email}}</p>
+            <p>Password:</p>
+            <p>Age:</p>
+            <p>Message:</p>
+            <p><strong>Send Mail?</strong></p>
+            <ul>
+              <li></li>
+            </ul>
+            <p>Gender:</p>
+            <p>Priority:</p>
+            <p>Switched:</p></p></p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      email: ""
+    };
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
