4767c1d971b84d4c2b967eb517a9579e82803e6e
Ayazadeh <m.ayazadeh@gmail.com>
2022-12-19 09:47:52 +0330

    Vue2: Registering Directives Locally ( directives hook )

--- FRON_END/Vuejs/15/src/App.vue
+++ FRON_END/Vuejs/15/src/App.vue
@@ -15,11 +15,17 @@
         <div
           class="col-xs-12 col-sm-8 col-sm-affset-2 col-md-6 col-md-offset-3"
         >
-          <h1 v-highlight:background.delayed="'lightblue'">Custom Directives</h1>
+          <p v-highlight:background.delayed="'lightblue'">
+            Custom Directives ( Global Directive )
+          </p>
           <!-- an argument passed with a colon ( v-highlight:arg="") -->
           <!-- Created Global Directive in main.js -->
           <!-- whatever we enter here ( v-highlight="''" )
-             between the quotation marks is the value of the binding  -->
+            between the quotation marks is the value of the binding  -->
+          <p v-local-highlight:background.delayed="'lightblue'">
+            Color this, too ( Local Direcvtive )
+          </p>
+          <!-- using local directive -->
         </div>
       </div>
     </div>
@@ -29,6 +35,27 @@
 <script>
 export default {
   name: "App",
+  // Registering a Local Directive
+  directives: {
+    "local-highlight": {
+      bind(el, binding, vnode) {
+        var delay = 0;
+        if (binding.modifiers["delayed"]) {
+          // set modifier to v-local-highlight => v-local-highlight:background.delayed(modifier)
+          delay = 3000;
+        }
+        setTimeout(() => {
+          if (binding.arg == "background") {
+            // set argument to v-local-highlight => v-local-highlight:background(argument)
+            el.style.backgroundColor = binding.value;
+          } else {
+            // fetch String value from binding object => v-local-highlight="'value'"
+            el.style.color = binding.value;
+          }
+        }, delay);
+      },
+    },
+  },
 };
 </script>
 
