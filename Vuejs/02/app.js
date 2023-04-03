69ae7f395002f4577e5390a74d333ad8b277f131
ayazadeh <50027@hfz.com>
2023-04-03 07:55:47 +0330

    Refactor

--- Vuejs/02/app.js
+++ Vuejs/02/app.js
@@ -1,5 +1,4 @@
 Vue.createApp({
-<<<<<<< HEAD
     data() {
         return{
             goals: [],
@@ -14,23 +13,6 @@ Vue.createApp({
     }
 }).mount('#app');
 
-
-=======
-  data() {
-    return {
-      goals: [],
-      enteredValue: "",
-    };
-  },
-  methods: {
-    addGoal() {
-      this.goals.push(this.enteredValue);
-      this.enteredValue = "";
-    },
-  },
-}).mount("#app");
->>>>>>> 6dde0640207e7c8b81c427c4009041f370bd7ba4
-
 // const buttonEl = document.querySelector('button');
 // const inputEl = document.querySelector('input');
 // const listEl = document.querySelector('ul');
@@ -43,8 +25,4 @@ Vue.createApp({
 //     inputEl.value = '';
 // }
 
-<<<<<<< HEAD
 // buttonEl.addEventListener('click', addGoal);
-=======
-// buttonEl.addEventListener('click', addGoal);
->>>>>>> 6dde0640207e7c8b81c427c4009041f370bd7ba4
