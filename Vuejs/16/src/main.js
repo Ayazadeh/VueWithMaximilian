58aed451f5b575ba86ad134b80b95f6b62aafa62
ayazadeh <50027@hfz.com>
2023-06-08 10:53:49 +0330

    Vue2: Creating a Global Mixin

--- Vuejs/16/src/main.js
+++ Vuejs/16/src/main.js
@@ -7,6 +7,14 @@ Vue.filter("toLowercase", function (value) {
   return value.toLowerCase();
 });
 
+Vue.mixin({
+  created() {
+    // execution order: Global Mixin > Local Mixin > component
+    // use Global Mixin with caution, it really affects everything
+    console.log("Global Mixin - Created Hook");
+  },
+});
+
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
