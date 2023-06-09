17c9222cda0d5f89e884dbfaae6d6aa1dba4c148
ayazadeh <m.ayazadeh@gmail.com>
2023-06-09 12:15:14 +0330

    Vue2: using <transition> element without animation

--- Vuejs/17/src/App.vue
+++ Vuejs/17/src/App.vue
@@ -1,8 +1,16 @@
 <template>
-  <div id="app">
+  <div class="container">
     <div class="row">
       <div class="col-sx-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animations</h1>
+        <hr />
+        <button class="btn btn-primary" @click="show = !show">
+          show Alert
+        </button>
+        <br /><br />
+        <transition>
+          <div class="alert alert-info" v-if="show">This is some Info</div>
+        </transition>
       </div>
     </div>
   </div>
@@ -11,6 +19,11 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      show: false,
+    };
+  },
 };
 </script>
 
