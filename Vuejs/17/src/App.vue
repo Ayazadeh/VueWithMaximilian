f47cedc6b665fff76ed81e70f6e2cc8c9051ccc4
ayazadeh <m.ayazadeh@gmail.com>
2023-06-10 13:15:40 +0330

    Vue2: Creating a _Slide_ Transition with the CSS Animation Property

--- Vuejs/17/src/App.vue
+++ Vuejs/17/src/App.vue
@@ -12,6 +12,9 @@
         <transition name="fade">
           <div class="alert alert-info" v-if="show">This is some Info</div>
         </transition>
+        <transition name="slide">
+          <div class="alert alert-info" v-if="show">This is some Info</div>
+        </transition>
       </div>
     </div>
   </div>
@@ -29,17 +32,49 @@ export default {
 </script>
 
 <style>
+/* using transition */
+
 .fade-enter {
   opacity: 0;
 }
 .fade-enter-active {
-  transition: opacity 2s;
+  transition: opacity 1s;
 }
 .fade-leave {
   /* opacity: 1; */
 }
 .fade-leave-active {
-  transition: opacity 2s;
+  transition: opacity 1s;
   opacity: 0;
+}
+
+/* using animation */
+
+.slide-enter {
+  /* transform: translateY(20px); */
+}
+.slide-enter-active {
+  animation: slide-in 1s ease-out forwards;
+}
+.slide-leave {
+}
+.slide-leave-active {
+  animation: slide-out 1s ease-out forwards;
+}
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(20px);
+  }
 }
 </style>
