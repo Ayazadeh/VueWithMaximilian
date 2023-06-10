a855f91c6af2eb3385860f560bd8534b11ef5836
ayazadeh <m.ayazadeh@gmail.com>
2023-06-10 15:37:13 +0330

    Vue2: Transitioning between Multiple Elements

--- Vuejs/17/src/App.vue
+++ Vuejs/17/src/App.vue
@@ -15,20 +15,29 @@
         <br /><br />
         <!-- only may use <transition> for a single element -->
         <transition :name="alertAnimation">
-          <div class="alert alert-info" v-if="show">This is some Info</div>
+          <div class="alert alert-info" v-if="show">This is some Info 1</div>
         </transition>
         <br />
         <transition name="slide" type="animation">
-          <div class="alert alert-info" v-if="show">This is some Info</div>
+          <div class="alert alert-info" v-if="show">This is some Info 2</div>
         </transition>
         <transition name="fade" appear>
-          <div class="alert alert-info" v-if="show">This is some Info</div>
+          <div class="alert alert-info" v-if="show">This is some Info 3</div>
         </transition>
         <transition
           enter-active-class="animate__animated animate__swing"
           leave-active-class="animate__animated animate__wobble"
         >
-          <div class="alert alert-info" v-if="show">This is some Info</div>
+          <div class="alert alert-info" v-if="show">This is some Info 4</div>
+        </transition>
+        <br />
+        <transition :name="alertAnimation" mode="out-in">
+          <div class="alert alert-info" v-if="show" key="info">
+            This is some Info 5
+          </div>
+          <div class="alert alert-warning" v-else key="warning">
+            This is some Warning
+          </div>
         </transition>
       </div>
     </div>
@@ -78,7 +87,7 @@ export default {
 }
 .slide-leave-active {
   animation: slide-out 1s ease-out forwards;
-  transition: opacity 3s;
+  transition: opacity 1s;
   opacity: 0;
 }
 @keyframes slide-in {
