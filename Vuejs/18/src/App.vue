25cbcfe5863687ba70552f5d41ebf6917e3e122f
ayazadeh <m.ayazadeh@gmail.com>
2023-06-12 22:14:16 +0330

    Vu2: add Answer & Question components

--- Vuejs/18/src/App.vue
+++ Vuejs/18/src/App.vue
@@ -2,15 +2,32 @@
   <div class="container">
     <div class="row">
       <div class="col-sx-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Animations</h1>
-        
+        <h1 class="text-center">The Super Quiz</h1>
+      </div>
+    </div>
+    <hr />
+    <div class="row">
+      <div class="col-sx-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <component :is="mode"></component>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Question from "./components/Question.vue";
+import Answer from "./components/Answer.vue";
+
 export default {
   name: "App",
+  components: {
+    appQuestion: Question,
+    appAnswer: Answer,
+  },
+  data() {
+    return {
+      mode: "app-question",
+    };
+  },
 };
 </script>
