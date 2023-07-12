fa7a0dae4690e168848742160e2c7a7c03004513
ayazadeh <50027@hfz.com>
2023-07-12 16:18:18 +0330

    Vue2: Creating an Application

--- Vuejs/19/src/App.vue
+++ Vuejs/19/src/App.vue
@@ -3,6 +3,15 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Http</h1>
+        <div class="form-group">
+          <label>username</label>
+          <input type="text" class="form-control" v-model="user.username" />
+        </div>
+        <div class="form-group">
+          <label>Mail</label>
+          <input type="text" class="form-control" v-model="user.email" />
+        </div>
+        <button class="btn btn-primary" @click="submit">Submit</button>
       </div>
     </div>
   </div>
@@ -11,6 +20,19 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      user: {
+        username: "",
+        email: "",
+      },
+    };
+  },
+  methods: {
+    submit() {
+      console.log(this.user);
+    },
+  },
 };
 </script>
 
