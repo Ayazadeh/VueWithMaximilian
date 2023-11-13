80f48be21bef564a22b2310928f0fd606d227a44
ayazadeh <m.ayazadeh@gmail.com>
2023-11-13 20:22:02 +0330

    Vue2: Intercepting Responses

--- Vuejs/19/src/main.js
+++ Vuejs/19/src/main.js
@@ -1,22 +1,24 @@
 import Vue from "vue";
-import VueResource from 'vue-resource'
+import VueResource from "vue-resource";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
-Vue.use(VueResource)
+Vue.use(VueResource);
 
 // we can use below code to define main URL globally, then all requests will be sent to this URL
 // Vue.http.options.root = "/";
 
-// intercepting requests
-// Vue.http.interceptors.push((request, next) => {
-//   console.log("request ", request);
-//   if (request.method == "POST") {
-//     request.method = "PUT";
-//   }
-//   next();
-// });
+// Intercepting Requests
+/* Vue.http.interceptors.push((request, next) => {
+  console.log("request ", request);
+  if (request.method == "POST") {
+    request.method = "PUT";
+  }
+  next((response) => {
+    response.json = () => {return {messages: response.body }} // Intercepting Responses
+  });
+}); */
 
 new Vue({
   render: (h) => h(App),
