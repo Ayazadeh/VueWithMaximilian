541a3fe4621809691cc724c68bfb13fc8f6e76c1
ayazadeh <m.ayazadeh@gmail.com>
2023-11-14 22:25:44 +0330

    Vue2: Setting up the VueJS Router (vue-router)

--- Vuejs/20/src/App.vue
+++ Vuejs/20/src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Routing</h1>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ Vuejs/20/src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>The Home Page</h1>
+    <hr />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit sed quos
+      nesciunt voluptas. Suscipit, magni accusamus. Architecto repellat quod
+      illum nihil, cum culpa explicabo, aut odio ipsum necessitatibus voluptatem
+      soluta.
+    </p>
+  </div>
+</template>
