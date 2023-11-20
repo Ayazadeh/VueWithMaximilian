f6e89d0a73dc535d15e69e197529884426e7ff41
ayazadeh <m.ayazadeh@gmail.com>
2023-11-20 21:59:57 +0330

    Vue2: Setting Up and Loading Routes

--- Vuejs/20/src/main.js
+++ Vuejs/20/src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App.vue";
+import { routes } from "./routes";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes,
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
