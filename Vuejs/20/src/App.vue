68d4ca6fd894477e7305501ed337cca5fbf89df2
ayazadeh <m.ayazadeh@gmail.com>
2023-11-22 21:26:53 +0330

    Vue2: Navigating with Router Links

--- Vuejs/20/src/App.vue
+++ Vuejs/20/src/App.vue
@@ -4,6 +4,7 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Routing</h1>
         <br>
+        <app-header></app-header>
         <router-view></router-view>
       </div>
     </div>
@@ -11,8 +12,12 @@
 </template>
 
 <script>
+import Header from './components/Header.vue';
 export default {
   name: "App",
+  components:{
+    appHeader: Header
+  }
 };
 </script>
 
---
+++ Vuejs/20/src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+  <ul class="nav nav-pills">
+    <li role="presentation"><router-link to="/">Home</router-link></li>
+    <li role="presentation"><router-link to="/user">User</router-link></li>
+  </ul>
+</template>
