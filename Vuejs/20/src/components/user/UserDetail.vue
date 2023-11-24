48a3ed756467ba9ff9f7630022a7e80accce862e
ayazadeh <m.ayazadeh@gmail.com>
2023-11-24 07:12:12 -0800

    Vue2: Making Router Links more Dynamic

--- Vuejs/20/src/components/user/UserDetail.vue
+++ Vuejs/20/src/components/user/UserDetail.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-        <h1>Some User Details</h1>
-        <p>User Loaded has ID: {{ $route.params.id }}</p>
-    </div>
+  <div>
+    <h1>Some User Details</h1>
+    <p>User Loaded has ID: {{ $route.params.id }}</p>
+    <router-link
+      tag="button"
+      class="btn btn-primary"
+      :to="'/user/' + $route.params.id + '/edit'"
+    >
+      user edit
+    </router-link>
+  </div>
 </template>
