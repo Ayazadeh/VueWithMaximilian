f7bd34cc15b4f9e774d5cc549839ef566d4b557f
ayazadeh <m.ayazadeh@gmail.com>
2023-11-24 05:05:31 -0800

    Vue2: Reacting to Changes in Route Parameters

--- Vuejs/20/src/components/Header.vue
+++ Vuejs/20/src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <ul class="nav nav-pills">
     <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
-    <router-link to="/user/10" tag="li" active-class="active"><a>User</a></router-link>
+    <router-link to="/user/1" tag="li" active-class="active"><a>User 1</a></router-link>
+    <router-link to="/user/2" tag="li" active-class="active"><a>User 2</a></router-link>
   </ul>
 </template>
