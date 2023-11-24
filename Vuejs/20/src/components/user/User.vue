7cfb8c5d79d9e673594bb42fb6171c738f9a78b7
ayazadeh <m.ayazadeh@gmail.com>
2023-11-24 06:08:33 -0800

    Vue2: Setting Up Child Routes (Nested Routes)

--- Vuejs/20/src/components/user/User.vue
+++ Vuejs/20/src/components/user/User.vue
@@ -2,22 +2,24 @@
   <div>
     <h1>The User Page</h1>
     <hr />
-    <p>Loaded ID: {{ id }}</p>
+    <!-- <p>Loaded ID: {{ id }}</p> -->
     <button @click="navigateToHome" class="btn btn-primary">Go To Home</button>
+    <hr>
+    <router-view></router-view>
   </div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      id: this.$route.params.id,
-    };
-  },
-  watch: {
-    $route(to, from) {
-      this.id = to.params.id
-    },
-  },
+  // data() {
+  //   return {
+  //     id: this.$route.params.id,
+  //   };
+  // },
+  // watch: {
+  //   $route(to, from) {
+  //     this.id = to.params.id
+  //   },
+  // },
   methods: {
     navigateToHome() {
       this.$router.push("/");
--- Vuejs/20/src/routes.js
+++ Vuejs/20/src/routes.js
@@ -1,7 +1,16 @@
 import User from "./components/user/User.vue";
+import UserStart from "./components/user/UserStart.vue";
+import UserDetail from "./components/user/UserDetail.vue";
+import UserEdit from "./components/user/UserEdit.vue";
 import Home from "./components/Home.vue";
 
 export const routes = [
-  { path: "", component: Home },
-  { path: "/user/:id", component: User }, // Dynamic Route
+{ path: "", component: Home },
+  // { path: "/user/:id", component: User }, // Dynamic Route
+  {path: "/user", component: User, children:[
+    { path: "", component: UserStart},
+    { path: ":id", component: UserDetail},
+    { path: ":id/edit", component: UserEdit}
+
+  ]}
 ];
