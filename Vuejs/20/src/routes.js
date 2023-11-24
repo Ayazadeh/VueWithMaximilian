0ff7290f5bd9f4f579f7dac2b8dfef5a71e7f4e6
ayazadeh <m.ayazadeh@gmail.com>
2023-11-24 09:51:11 -0800

    Vue2: Multiple Router Views (Named Router Views)

--- Vuejs/20/src/routes.js
+++ Vuejs/20/src/routes.js
@@ -3,14 +3,27 @@ import UserStart from "./components/user/UserStart.vue";
 import UserDetail from "./components/user/UserDetail.vue";
 import UserEdit from "./components/user/UserEdit.vue";
 import Home from "./components/Home.vue";
+import Header from "./components/Header.vue";
 
 export const routes = [
-  { path: "", component: Home, name: "home" },
+  {
+    path: "",
+    name: "home",
+    // component: Home,
+    components: {
+      default: Home,
+      "Header-top": Header,
+    },
+  },
   // { path: "/user/:id", component: User }, // Dynamic Route
   // below code instead up code (nested route)
   {
     path: "/user",
-    component: User,
+    // component: User,
+    components: {
+      default: User,
+      "Header-bottom": Header,
+    },
     children: [
       { path: "", component: UserStart },
       { path: ":id", component: UserDetail },
