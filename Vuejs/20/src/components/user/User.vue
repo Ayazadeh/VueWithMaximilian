74c3cbaae5bb3f20bf4aa1e62f5dc728b540c93b
ayazadeh <m.ayazadeh@gmail.com>
2023-11-24 04:40:31 -0800

    Vue2: Navigating from Code (Imperative Navigation)

--- Vuejs/20/src/components/user/User.vue
+++ Vuejs/20/src/components/user/User.vue
@@ -1,3 +1,16 @@
 <template>
-    <h1>The User Page</h1>
+    <div>
+        <h1>The User Page</h1>
+        <hr>
+        <button @click="navigateToHome" class="btn btn-primary">Go To Home</button>
+    </div>
 </template>
+<script>
+export default{
+    methods:{
+        navigateToHome(){
+            this.$router.push('/')
+        }
+    }
+}
+</script>
