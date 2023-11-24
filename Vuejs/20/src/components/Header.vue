c7e29830e59f4ade9cfab06b47ce3b7131bbfcc3
ayazadeh <m.ayazadeh@gmail.com>
2023-11-24 03:15:49 -0800

    Vue2: Styling Active Links

--- Vuejs/20/src/components/Header.vue
+++ Vuejs/20/src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="nav nav-pills">
-    <li role="presentation"><router-link to="/">Home</router-link></li>
-    <li role="presentation"><router-link to="/user">User</router-link></li>
+    <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+    <router-link to="/user" tag="li" active-class="active"><a>User</a></router-link>
   </ul>
 </template>
