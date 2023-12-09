702f5e32550d7b00756afa01b338f0da6987da2b
ayazadeh <m.ayazadeh@gmail.com>
2023-12-09 23:10:07 +0330

    Vue2: Passing the Hash Fragment

--- Vuejs/20/src/components/user/UserDetail.vue
+++ Vuejs/20/src/components/user/UserDetail.vue
@@ -9,18 +9,28 @@
     >
       user edit
     </router-link> -->
-    <router-link
-      tag="button"
-      class="btn btn-primary"
-      :to="{
-        name: 'userEdit',
-        params: { id: $route.params.id },
-        query: { locale: 'en', q: 100 },
-      }">
-
+    <router-link tag="button" class="btn btn-primary" :to="link">
       user edit
-      
     </router-link>
-    <br><br>
+    <br /><br />
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      link: {
+        name: "userEdit",
+        params: {
+          id: this.$route.params.id,
+        },
+        query: {
+          locale: "en",
+          q: 100,
+        },
+        hash: "#data",
+      },
+    };
+  },
+};
+</script>
