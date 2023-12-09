cdd54734ef91d692281f16a97f116ac936139afb
ayazadeh <m.ayazadeh@gmail.com>
2023-12-09 23:26:45 +0330

    Vue2: Protecting Routes with Guards

--- Vuejs/20/src/components/user/UserEdit.vue
+++ Vuejs/20/src/components/user/UserEdit.vue
@@ -3,6 +3,8 @@
     <h3>Edit the User</h3>
     <p>Locale: {{ $route.query.locale }}</p>
     <p>Analytics: {{ $route.query.q }}</p>
+    <hr>
+    <button class="btn btn-primary">Confirm</button>
     <div style="height: 700px"></div>
     <h1 id="data">Some extera Data</h1>
   </div>
