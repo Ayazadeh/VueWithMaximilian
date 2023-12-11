022df4044c738806f7fbe6a60685ad61e9e1d297
ayazadeh <m.ayazadeh@gmail.com>
2023-12-11 22:47:05 +0330

    Vue2: Using the _beforeRouteLeave_ Guard

--- Vuejs/20/src/components/user/UserDetail.vue
+++ Vuejs/20/src/components/user/UserDetail.vue
@@ -35,7 +35,7 @@ export default {
   beforeRouteEnter(to, from, next) {
     // this is how we can check if a user allowed to visit a certain page.
     console.log("beforeRouteEnter in UserDetail.vue");
-    if (false) {
+    if (true) {
       next(); // let the request continue its journey
     } else {
       next(false); // abort request;
--- Vuejs/20/src/components/user/UserEdit.vue
+++ Vuejs/20/src/components/user/UserEdit.vue
@@ -3,9 +3,29 @@
     <h3>Edit the User</h3>
     <p>Locale: {{ $route.query.locale }}</p>
     <p>Analytics: {{ $route.query.q }}</p>
-    <hr>
-    <button class="btn btn-primary">Confirm</button>
+    <hr />
+    <button class="btn btn-primary" @click="confirmed = true">Confirm</button>
     <div style="height: 700px"></div>
     <h1 id="data">Some extera Data</h1>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      confirmed: false,
+    };
+  },
+  beforeRouteLeave(to, from, next) {
+    if (this.confirmed) {
+      next();
+    } else {
+      if (confirm("Are you sure?")) {
+        next();
+      } else {
+        next(false);
+      }
+    }
+  },
+};
+</script>
