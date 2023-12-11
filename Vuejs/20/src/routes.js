bac5055d7a9ed4b1fe811bf60deff343679de0c8
ayazadeh <m.ayazadeh@gmail.com>
2023-12-11 23:39:41 +0330

    Vue2: Loading Routes Lazily

--- Vuejs/20/src/routes.js
+++ Vuejs/20/src/routes.js
@@ -1,9 +1,52 @@
-import User from "./components/user/User.vue";
-import UserStart from "./components/user/UserStart.vue";
-import UserDetail from "./components/user/UserDetail.vue";
-import UserEdit from "./components/user/UserEdit.vue";
+// import User from "./components/user/User.vue";
+// import UserStart from "./components/user/UserStart.vue";
+// import UserDetail from "./components/user/UserDetail.vue";
+// import UserEdit from "./components/user/UserEdit.vue";
 import Home from "./components/Home.vue";
 import Header from "./components/Header.vue";
+
+/*
+
+using Lazy-Loading with webpack, which allows components
+to be loaded on-demand rather than upfront
+
+with lazy-loading, components are only rendered when needed, one by one.
+
+If you want to render multiple components at once, you
+can use grouping (e.g. into a 'userGroup').
+
+*/
+const User = (resolve) => {
+  require.ensure(
+    ["./components/user/User.vue"],
+    () => {
+      resolve(require("./components/user/User.vue"));
+    },
+    "userGroup"
+  );
+};
+
+const UserStart = (resolve) => {
+  require.ensure(
+    ["./components/user/UserStart.vue"],
+    () => {
+      resolve(require("./components/user/UserStart.vue"));
+    },
+    "userGroup"
+  );
+};
+
+const UserDetail = (resolve) => {
+  require.ensure(["./components/user/UserDetail.vue"], () => {
+    resolve(require("./components/user/UserDetail.vue"));
+  });
+};
+
+const UserEdit = (resolve) => {
+  require.ensure(["./components/user/UserEdit.vue"], () => {
+    resolve(require("./components/user/UserEdit.vue"));
+  });
+};
 
 export const routes = [
   {
