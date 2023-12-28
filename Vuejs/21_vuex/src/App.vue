4afa1e9802765fd69332adbf3b9703cdcb1eeadc
ayazadeh <m.ayazadeh@gmail.com>
2023-12-28 07:59:04 -0800

    Vue2: remove project 21 & use 21_vuex instead

--- Vuejs/21_vuex/src/App.vue
+++ Vuejs/21_vuex/src/App.vue
@@ -1,14 +1,31 @@
 <template>
   <div id="app">
-    
+    <div class="container">
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
+        >
+          <h1>Vuex</h1>
+          <app-result :counter="counter"></app-result>
+          <hr />
+          <app-counter @updated="counter += $event"></app-counter>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import CounterNumber from './components/CounterNumber.vue';
+import ResultNumber from './components/ResultNumber.vue';
 
 export default {
-  name: 'App',
-}
+  name: "App",
+  components:{
+    appCounter: CounterNumber,
+    appResult: ResultNumber
+  }
+};
 </script>
 
 <style>
