d17f4f9a4ca38c33d58d67bb4fff2e46d42ecd7a
ayazadeh <m.ayazadeh@gmail.com>
2024-02-25 09:05:56 -0800

    Vue2: Adding a Header and Navigation

--- Vuejs/22_final_project/src/App.vue
+++ Vuejs/22_final_project/src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="container">
-    <router-view></router-view>
+    <app-header />
+    <div class="row">
+      <div class="col-xs-12">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue';
 
 export default {
   name: 'App',
+  components:{
+    AppHeader,
+  }
 }
 </script>
+<style>
+body{
+  padding: 30px;
+}
+</style>
--- Vuejs/22_final_project/src/components/AppHeader.vue
+++ Vuejs/22_final_project/src/components/AppHeader.vue
@@ -1,3 +1,52 @@
 <template>
-    <h1>The Header Component</h1>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <router-link to="/" class="navbar-brand"
+          >Stock Trader</router-link
+        >
+      </div>
+
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <router-link
+            tag="li"
+            to="/portfolio"
+            activeClass="active"
+          >
+            <a>Portfolio</a>
+          </router-link>
+          <router-link
+            tag="li"
+            to="/stocks"
+            activeClass="active"
+          >
+            <a>Stocks</a>
+          </router-link>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">End Day</a></li>
+          <li class="dropdown">
+            <a
+              href="#"
+              role="button"
+              aria-haspopup="true"
+              aria-expanded="false"
+              data-toggle="dropdown"
+              class="dropdown-toggle"
+            >
+              Save & Load
+              <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Save Data</a></li>
+              <li><a href="#">Load Data</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+  </nav>
 </template>
