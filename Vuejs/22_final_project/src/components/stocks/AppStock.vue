d71fa22cc99a7c82bc3ca1ed476cfdecbbc6a471
ayazadeh <m.ayazadeh@gmail.com>
2024-02-25 09:26:12 -0800

    Vue2: Creating Stocks Components

--- Vuejs/22_final_project/src/components/stocks/AppStock.vue
+++ Vuejs/22_final_project/src/components/stocks/AppStock.vue
@@ -1,3 +1,26 @@
 <template>
-    <h1>The Stocks - Stock Component</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="panel panel-success">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          Name
+          <small>(Price: PRICE)</small>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input
+            type="number"
+            class="form-control"
+            placeholder="Quantity"
+          />
+        </div>
+        <div class="pull-right">
+          <button class="btn btn-success">
+            Buy
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
--- Vuejs/22_final_project/src/components/stocks/AppStocks.vue
+++ Vuejs/22_final_project/src/components/stocks/AppStocks.vue
@@ -1,3 +1,24 @@
 <template>
-    <h1>The Stocks Component</h1>
+  <div>
+    <app-stock :key="stock" v-for="stock in stocks" />
+  </div>
 </template>
+<script>
+import AppStock from './AppStock.vue';
+export default{
+    components:{
+        AppStock,
+    },
+    data(){
+        return{
+            stocks:[
+                {id: 1, name: "BMW", price: 110},
+                {id: 2, name: "Google", price: 200},
+                {id: 3, name: "Apple", price: 220},
+                {id: 4, name: "Twitter", price: 100},
+                {id: 5, name: "Telegram", price: 130},
+            ]
+        }
+    }
+}
+</script>
