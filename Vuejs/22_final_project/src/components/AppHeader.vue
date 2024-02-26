195fa62beba20635c2c5a86d1bd53aa75ced6aaa
ayazadeh <m.ayazadeh@gmail.com>
2024-02-26 05:25:02 -0800

    Vue2: Displaying the Funds

--- Vuejs/22_final_project/src/components/AppHeader.vue
+++ Vuejs/22_final_project/src/components/AppHeader.vue
@@ -2,28 +2,19 @@
   <nav class="navbar navbar-default">
     <div class="container-fluid">
       <div class="navbar-header">
-        <router-link to="/" class="navbar-brand"
-          >Stock Trader</router-link
-        >
+        <router-link to="/" class="navbar-brand">Stock Trader</router-link>
       </div>
 
       <div class="collapse navbar-collapse">
         <ul class="nav navbar-nav">
-          <router-link
-            tag="li"
-            to="/portfolio"
-            activeClass="active"
-          >
+          <router-link tag="li" to="/portfolio" activeClass="active">
             <a>Portfolio</a>
           </router-link>
-          <router-link
-            tag="li"
-            to="/stocks"
-            activeClass="active"
-          >
+          <router-link tag="li" to="/stocks" activeClass="active">
             <a>Stocks</a>
           </router-link>
         </ul>
+        <strong class="navbar-text navbar-right"> Founds: {{ funds }} </strong>
         <ul class="nav navbar-nav navbar-right">
           <li><a href="#">End Day</a></li>
           <li class="dropdown">
@@ -50,3 +41,12 @@
     <!-- /.container-fluid -->
   </nav>
 </template>
+<script>
+export default{
+  computed:{
+    funds(){
+      return this.$store.getters.funds;
+    }
+  }
+}
+</script>
