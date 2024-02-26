e47cc95120d2dbe2aa3898445af3285f6be62792
ayazadeh <m.ayazadeh@gmail.com>
2024-02-26 05:11:47 -0800

    Vue2: fix sell order

--- Vuejs/22_final_project/src/components/portfolio/AppStock.vue
+++ Vuejs/22_final_project/src/components/portfolio/AppStock.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-sm-6 col-md-4">
-    <div class="panel panel-success">
+    <div class="panel panel-info">
       <div class="panel-heading">
         <h3 class="panel-title">
           {{ stock.name }}
@@ -20,7 +20,7 @@
         </div>
         <div class="pull-right">
           <button
-            class="btn btn-success"
+            class="btn btn-warning"
             @click="sellStock"
             :disabled="quantity <= 0 || !Number.isInteger(quantity)"
           >
@@ -46,16 +46,17 @@ export default {
         }
     },
     methods:{
-        ...mapActions([
-            'sellStock'
-        ]),
+        ...mapActions({
+          placeSellOrder: 'sellStock'
+        }),
         sellStock(){
           const order = {
               stockId: this.stock.id,
               stockPrice: this.stock.price,
               quantity: this.quantity
           }
-          this.sellStock(order)
+          this.placeSellOrder(order)
+          this.quantity = 0;
       }
     }
 }
