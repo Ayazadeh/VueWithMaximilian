a97834357dd917fda03e9a0a054e17003aa2ea3b
ayazadeh <m.ayazadeh@gmail.com>
2024-02-26 04:26:04 -0800

    Vue2: Adding a Portfolio Module to Vuex

--- Vuejs/22_final_project/src/store/store.js
+++ Vuejs/22_final_project/src/store/store.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import stocks from "./modules/stocks";
+import portfolio from "./modules/portfolio";
 
 Vue.use(Vuex);
 
@@ -9,5 +10,6 @@ export default new Vuex.Store({
   modules: {
     // register modules
     stocks,
+    portfolio,
   },
 });
