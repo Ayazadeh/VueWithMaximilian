0198fc087752cb586fd843b047e84192977db61b
ayazadeh <m.ayazadeh@gmail.com>
2024-03-08 19:12:27 +0330

    Vue2: install axios

--- Vuejs/23_axios/src/components/auth/signup.vue
+++ Vuejs/23_axios/src/components/auth/signup.vue
@@ -104,6 +104,9 @@ import axios from 'axios'
           terms: this.terms
         }
         console.log(formData)
+        axios.post('https://vue-backend-48df1-default-rtdb.firebaseio.com/users.json', formData)
+        .then(res => console.log(res))
+        .catch(error => console.log(error))
       }
     }
   }
