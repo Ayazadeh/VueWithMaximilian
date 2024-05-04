8bb0172592c269b4a5b518288f119c3911c894b9
ayazadeh <m.ayazadeh@gmail.com>
2024-05-04 01:53:41 -0700

    Vue3: first project => First Components & Props

---
+++ Vuejs/24_vue3_first_project/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,19 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>Delete</button>
+      </header>
+    </div>
+    <p>{{  description }}</p>
+    <nav>
+      <a :href="link">Vue Resource</a>
+    </nav>
+  </li>
+</template>
+<script>
+export default{
+    props: ['title', 'description', 'link']
+}
+</script>
