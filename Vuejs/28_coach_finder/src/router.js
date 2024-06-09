8b4de4e4ed39f62c4b7d523ec4d777c05eb70503
ayazadeh <m.ayazadeh@gmail.com>
2024-06-10 00:06:13 +0330

    Vue3: add project 28

---
+++ Vuejs/28_coach_finder/src/router.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import CoachDetail from "./views/coaches/CoachDetail.vue";
+import CoachesList from "./views/coaches/CoachesList.vue";
+import CoachRegistration from "./views/coaches/CoachRegistration.vue";
+import ContactCoach from "./views/requests/ContactCoach.vue";
+import RequestsReceived from "./views/requests/RequestsReceived.vue";
+import NotFound from "./views/NotFound.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches'},
+    { path: "/coaches", component: CoachesList },
+    {
+      path: "/coaches/:id",
+      component: CoachDetail,
+      children: [{ path: "contact", component: ContactCoach }],
+    },
+    { path: "/register", component: CoachRegistration },
+    { path: "/requests", component: RequestsReceived },
+    { path: "/:notFound(.*)", component: NotFound },
+  ],
+});
+
+export default router;
