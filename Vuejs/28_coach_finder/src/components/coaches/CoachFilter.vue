226239953a8aa9165bb0b1e4928c311cf8747ac8
ayazadeh <m.ayazadeh@gmail.com>
2024-06-14 00:36:25 +0330

    Vue3: project 28 => add Filtering Coaches

---
+++ Vuejs/28_coach_finder/src/components/coaches/CoachFilter.vue
@@ -0,0 +1,68 @@
+<
+<template>
+  <base-card>
+    <h2>Find Your Coach</h2>
+    <span class="filter-option">
+      <input type="checkbox" id="frontend" checked @change="setFilters" />
+      <label for="frontend">Front-End</label>
+    </span>
+    <span class="filter-option">
+      <input type="checkbox" id="backend" checked @change="setFilters" />
+      <label for="backend">Back-End</label>
+    </span>
+    <span class="filter-option">
+      <input type="checkbox" id="career" checked @change="setFilters" />
+      <label for="career">Career</label>
+    </span>
+  </base-card>
+</template>
+
+<script>
+export default {
+  emits: ["change-filter"],
+  data() {
+    return {
+      filters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
+  },
+  methods: {
+    setFilters(event) {
+      const inputId = event.target.id;
+      const isActive = event.target.checked;
+      const updatedFilters = {
+        ...this.filters,
+        [inputId]: isActive,
+      };
+      this.filters = updatedFilters;
+      this.$emit("change-filter", updatedFilters);
+    },
+  },
+};
+</script>
+
+<style scoped>
+h2 {
+  margin: 0.5rem 0;
+}
+
+.filter-option {
+  margin-right: 1rem;
+}
+
+.filter-option label,
+.filter-option input {
+  vertical-align: middle;
+}
+
+.filter-option label {
+  margin-left: 0.25rem;
+}
+
+.filter-option.active label {
+  font-weight: bold;
+}
+</style>
