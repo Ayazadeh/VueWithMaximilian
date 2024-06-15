e22010de562afb46a05ad0400704d89612ac843e
ayazadeh <m.ayazadeh@gmail.com>
2024-06-15 22:53:04 +0330

    vue3: project 28 => Registering as a Coach_ The Form

--- Vuejs/28_coach_finder/src/views/coaches/CoachRegistration.vue
+++ Vuejs/28_coach_finder/src/views/coaches/CoachRegistration.vue
@@ -1,6 +1,17 @@
 <template>
-    Register
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
 <script>
+import CoachForm from "@/components/coaches/CoachForm";
 
+export default {
+  components: {
+    CoachForm,
+  },
+};
 </script>
