d3c6ee3cb59cba307f36bfb72b4d88a1a51e8d0c
ayazadeh <m.ayazadeh@gmail.com>
2024-06-17 15:16:40 +0330

    Vue3: project 28 => Working on the Contact Form

--- Vuejs/28_coach_finder/src/views/requests/ContactCoach.vue
+++ Vuejs/28_coach_finder/src/views/requests/ContactCoach.vue
@@ -1,6 +1,86 @@
 <template>
-    Contact page
+  <form @submit.prevent="submitForm">
+    <div class="form-control">
+      <label for="email">Email</label>
+      <input type="email" id="email" v-model.trim="email" />
+    </div>
+    <div class="form-control">
+      <label for="message">Message</label>
+      <textarea id="message" rows="5" v-model.trim="message" />
+    </div>
+    <p class="errors" v-if="!formIsValid">
+      Please enter a valid Email and non-emtpty Message
+    </p>
+    <div class="actions">
+      <base-button>Send Message</base-button>
+    </div>
+  </form>
 </template>
 <script>
-
+export default {
+  data() {
+    return {
+      email: "",
+      message: "",
+      formIsValid: true,
+    };
+  },
+  methods: {
+    submitForm() {
+      this.formIsValid = true;
+      if (
+        this.email === "" ||
+        !this.email.includes("@") ||
+        this.message === ""
+      ) {
+        this.formIsValid = false;
+        return;
+      }
+    },
+  },
+};
 </script>
+
+<style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
+</style>
