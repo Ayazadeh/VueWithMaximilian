72018bf84a5c603d6cd48a9d5b64cf9d5fd35312
ayazadeh <m.ayazadeh@gmail.com>
2024-06-19 22:54:58 +0330

    Vue3: project 28 => Ref

--- Vuejs/28_coach_finder/src/store/modules/requests/getters.js
+++ Vuejs/28_coach_finder/src/store/modules/requests/getters.js
@@ -1,6 +1,7 @@
 export default {
   requests: (state, getters, rootState, rootGetters) =>
     state.requests.filter((req) => req.coachId === rootGetters.userId),
+
   hasRequests: (state, getters) =>
     getters.requests && getters.requests.length > 0,
 };
