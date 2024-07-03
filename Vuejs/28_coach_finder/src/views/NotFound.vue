dfc752d01f8da488354cdc4b770798a19d13b549
ayazadeh <m.ayazadeh@gmail.com>
2024-07-03 23:31:35 +0330

    Vue3: project 28 => The _Not Found_ Page

--- Vuejs/28_coach_finder/src/views/NotFound.vue
+++ Vuejs/28_coach_finder/src/views/NotFound.vue
@@ -1,6 +1,12 @@
 <template>
-    
+  <section>
+    <base-card>
+      <h2>Page not Found</h2>
+      <p>
+        This page could not be found - maybe check out all our
+        <router-link to="/">coaches</router-link>.
+      </p>
+    </base-card>
+  </section>
 </template>
-<script>
-
-</script>
+<script></script>
