eaeca58a48238e8d8103c85ccdf7f0ea6522109a
ayazadeh <m.ayazadeh@gmail.com>
2024-07-05 22:04:55 +0330

    Vue3: project 28 => add auth to store for authentication

--- Vuejs/28_coach_finder/src/store/index.js
+++ Vuejs/28_coach_finder/src/store/index.js
@@ -2,18 +2,12 @@ import { createStore } from "vuex";
 
 import coachesModule from "./modules/coaches/index";
 import requestsModule from "./modules/requests/index";
+import authModule from "./modules/auth/index";
 
 export default createStore({
   modules: {
     coaches: coachesModule,
     requests: requestsModule,
+    auth: authModule,
   },
-  state: {
-    userId: "c3",
-  },
-  getters: {
-    userId: (state) => state.userId,
-  },
-  mutations: {},
-  actions: {},
 });
---
+++ Vuejs/28_coach_finder/src/store/modules/auth/index.js
@@ -0,0 +1,12 @@
+import mutations from "./mutations";
+import actions from "./actions";
+import getters from "./getters";
+
+export default {
+  state: {
+    userId: "c3",
+  },
+  mutations,
+  actions,
+  getters,
+};
