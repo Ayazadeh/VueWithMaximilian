214870f95cad508bd97117607436878ade40ca5d
ayazadeh <m.ayazadeh@gmail.com>
2024-07-09 22:37:01 +0330

    Vue3: project 28 => Adding a _Login_ Action & Flow

--- Vuejs/28_coach_finder/src/store/modules/auth/actions.js
+++ Vuejs/28_coach_finder/src/store/modules/auth/actions.js
@@ -1,5 +1,32 @@
 export default {
-  login() {},
+  async login(context, payload) {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1//accounts:signInWithPassword?key=AIzaSyCp3qvG-odvraBgm14HEsnPXJOL-KnRP50",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const responseData = response.json();
+
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || "Failed to authenticate. Check your login data."
+      );
+      throw error;
+    }
+
+    context.commit("setUser", {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
   async signup(context, payload) {
     const response = await fetch(
       "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCp3qvG-odvraBgm14HEsnPXJOL-KnRP50",
@@ -22,10 +49,10 @@ export default {
       throw error;
     }
 
-    context.commit('setUser', {
-        token: responseData.idToken,
-        userId: responseData.localId,
-        tokenExpiration: responseData.expiresIn
-    })
+    context.commit("setUser", {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
   },
 };
--- Vuejs/28_coach_finder/src/views/auth/UserAuth.vue
+++ Vuejs/28_coach_finder/src/views/auth/UserAuth.vue
@@ -70,14 +70,16 @@ export default {
 
       this.isLoading = true;
 
+      const actionPayload = {
+        email: this.email,
+        password: this.password,
+      };
+
       try {
         if (this.mode === "login") {
-          //...
+          await this.$store.dispatch("login", actionPayload);
         } else {
-          await this.$store.dispatch("signup", {
-            email: this.email,
-            password: this.password,
-          });
+          await this.$store.dispatch("signup", actionPayload);
         }
       } catch (error) {
         this.error = error.message || "Failed to authenticated, try later.";
