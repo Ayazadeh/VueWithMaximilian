30f22d9f01de91ef54ffd2ad4f12f940684c0472
ayazadeh <50027@hfz.com>
2024-07-29 19:00:29 +0330

    Fix: project 27_vuex

--- Vuejs/27_vuex/src/store/modules/products.js
+++ Vuejs/27_vuex/src/store/modules/products.js
@@ -15,7 +15,7 @@ export default {
         {
           id: 'p2',
           image:
-            'https://upload.wikimedia.org/wikipedia/en/thumb/c/c9/Tent_at_High_Shelf_Camp_cropped.jpg/640px-Tent_at_High_Shelf_Camp_cropped.jpg',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Tent_at_High_Shelf_Camp_cropped.jpg/640px-Tent_at_High_Shelf_Camp_cropped.jpg',
           title: 'Mountain Tent',
           description: 'A tent for the ambitious outdoor tourist.',
           price: 129.99,
