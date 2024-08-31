665f9b3e756f71a2458da8dc2a89bd4b3b3e6f59
ayazadeh <m.ayazadeh@gmail.com>
2024-08-31 21:29:21 +0330

    Vue3: project 30 => use composable useRoute from vue-router instead of props

--- Vuejs/30_composition-api/src/pages/ProductDetails.vue
+++ Vuejs/30_composition-api/src/pages/ProductDetails.vue
@@ -9,13 +9,15 @@
 
 <script>
 import { inject, computed } from "vue";
+import { useRoute } from 'vue-router'
 
 export default {
-  props: ['pid'],
-  setup(props) {
+  setup() {
     const products = inject("products");
+
+    const route = useRoute()
     
-    const selectedProduct = computed(() => products.value.find(product => product.id === props.pid));
+    const selectedProduct = computed(() => products.value.find(product => product.id === route.params.pid));
     const title = computed(() => selectedProduct.value.title);
     const price = computed(() => selectedProduct.value.price);
     const description = computed(() => selectedProduct.value.description);
