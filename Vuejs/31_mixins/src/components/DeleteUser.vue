0f7f4ac45b755f5e1cfcd5c3f33ffc7b822abfd9
ayazadeh <m.ayazadeh@gmail.com>
2024-09-04 11:35:08 +0330

    Vue3: project 31 => Using Mixins

--- Vuejs/31_mixins/src/components/DeleteUser.vue
+++ Vuejs/31_mixins/src/components/DeleteUser.vue
@@ -9,24 +9,13 @@
 </template>
 
 <script>
-import UserAlert from './UserAlert.vue';
+import UserAlert from "./UserAlert.vue";
+import alertMixin from "../mixins/alert";
 
 export default {
   components: {
     UserAlert,
   },
-  data() {
-    return {
-      alertIsVisible: false,
-    };
-  },
-  methods: {
-    showAlert() {
-      this.alertIsVisible = true;
-    },
-    hideAlert() {
-      this.alertIsVisible = false;
-    },
-  },
+  mixins: [alertMixin],
 };
 </script>
