1dbd7f788239ee65d80ca85e331030af082ad246
ayazadeh <m.ayazadeh@gmail.com>
2024-09-04 11:50:18 +0330

    Vue3: project 31 => Global Mixins

--- Vuejs/31_mixins/src/main.js
+++ Vuejs/31_mixins/src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import loggerMixin from './mixins/logger'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.mixin(loggerMixin)
+
+app.mount('#app')
