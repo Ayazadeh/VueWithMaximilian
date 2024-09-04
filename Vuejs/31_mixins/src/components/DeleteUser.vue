d1ef7e76e058e6b273d769a89f41d8c0484852ed
ayazadeh <m.ayazadeh@gmail.com>
2024-09-04 11:43:48 +0330

    Vue3: project 31 => Understanding Mixin Merging

--- Vuejs/31_mixins/src/components/DeleteUser.vue
+++ Vuejs/31_mixins/src/components/DeleteUser.vue
@@ -1,5 +1,5 @@
 <template>
-  <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
+  <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
     <p>Do you want to continue with deleting a user?</p>
   </user-alert>
   <section>
@@ -15,6 +15,12 @@ import alertMixin from "../mixins/alert";
 export default {
   components: {
     UserAlert,
+  },
+  data() {
+    return {
+      alertTitle: 'Delete User?',
+      alertIsVisible: true,
+    }
   },
   mixins: [alertMixin],
 };
